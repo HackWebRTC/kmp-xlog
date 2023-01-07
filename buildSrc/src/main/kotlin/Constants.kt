72ffb4f1982adbc957acdc2a8e24ba50be067cbc
Piasy <xz4215@gmail.com>
2023-01-07 19:15:23 +0800

    wip: windows

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -15,7 +15,7 @@ object Consts {
 
   const val releaseGroup = "com.piasy"
   const val releaseName = "kmp-xlog"
-  const val releaseVersion = "1.0.0"
+  const val releaseVersion = "1.1.0"
 
   val androidNS = "$releaseGroup.${releaseName.replace('-', '_')}"
 }
