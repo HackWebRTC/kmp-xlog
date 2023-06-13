2835a012e4d87d565d5d87931c4ffaae77d8f482
Piasy <xz4215@gmail.com>
2023-06-14 00:04:00 +0800

    add cocoapods release note

--- publish_apple_cocoapods.sh
+++ publish_apple_cocoapods.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# pod trunk register 'xz4215@gmail.com' 'kmp_xlog'
+# then open email to verify the register
+
 pushd kmp-xlog/build/cocoapods/publish/release
 pod trunk push kmp_xlog.podspec --allow-warnings
 popd
